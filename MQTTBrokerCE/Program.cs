279e6ac0d3d2df34a2048158d1192f3dabd5248e
cadom <cadom@DESKTOP-R639IM0>
2018-09-20 21:45:23 -0400

    Add project files.

---
+++ MQTTBrokerCE/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+using MQTTnet.Diagnostics;
+using Newtonsoft.Json;
+
+namespace MQTTBrokerCE
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"MQTTnet - TestApp.{TargetFrameworkInfoProvider.TargetFramework}");
+            Console.WriteLine("1 = Start client");
+
+            var pressedKey = Console.ReadKey(true);
+            if (pressedKey.KeyChar == '1')
+            {
+                Task.Run(RunServer);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        public static async Task RunServer()
+        {
+            try
+            {
+                var options = new MqttServerOptions
+                {
+                    ConnectionValidator = p =>
+                    {
+                        if (p.ClientId == "SpecialClient")
+                        {
+                            if (p.Username != "USER" || p.Password != "PASS")
+                            {
+                               // p.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                            }
+                        }
+                    },
+
+                    Storage = new RetainedMessageHandler(),
+
+                    ApplicationMessageInterceptor = context =>
+                    {
+                        if (MqttTopicFilterComparer.IsMatch(context.ApplicationMessage.Topic, "/myTopic/WithTimestamp/#"))
+                        {
+                            // Replace the payload with the timestamp. But also extending a JSON 
+                            // based payload with the timestamp is a suitable use case.
+                            context.ApplicationMessage.Payload = Encoding.UTF8.GetBytes(DateTime.Now.ToString("O"));
+                        }
+
+                        if (context.ApplicationMessage.Topic == "not_allowed_topic")
+                        {
+                            context.AcceptPublish = false;
+                            context.CloseConnection = true;
+                        }
+                    },
+                    SubscriptionInterceptor = context =>
+                    {
+                        if (context.TopicFilter.Topic.StartsWith("admin/foo/bar") && context.ClientId != "theAdmin")
+                        {
+                            context.AcceptSubscription = false;
+                        }
+
+                        if (context.TopicFilter.Topic.StartsWith("the/secret/stuff") && context.ClientId != "Imperator")
+                        {
+                            context.AcceptSubscription = false;
+                            context.CloseConnection = true;
+                        }
+                    }
+                };
+
+                // Extend the timestamp for all messages from clients.
+                // Protect several topics from being subscribed from every client.
+
+                //var certificate = new X509Certificate(@"C:\certs\test\test.cer", "");
+                //options.TlsEndpointOptions.Certificate = certificate.Export(X509ContentType.Cert);
+                //options.ConnectionBacklog = 5;
+                //options.DefaultEndpointOptions.IsEnabled = true;
+                //options.TlsEndpointOptions.IsEnabled = false;
+
+                var mqttServer = new MqttFactory().CreateMqttServer();
+
+                mqttServer.ApplicationMessageReceived += (s, e) =>
+                {
+                    Console.Write(
+                        $"'{e.ClientId}' reported '{e.ApplicationMessage.Topic}' > '{Encoding.UTF8.GetString(e.ApplicationMessage.Payload ?? new byte[0])}'",
+                        ConsoleColor.Magenta);
+                };
+
+                //options.ApplicationMessageInterceptor = c =>
+                //{
+                //    if (c.ApplicationMessage.Payload == null || c.ApplicationMessage.Payload.Length == 0)
+                //    {
+                //        return;
+                //    }
+
+                //    try
+                //    {
+                //        var content = JObject.Parse(Encoding.UTF8.GetString(c.ApplicationMessage.Payload));
+                //        var timestampProperty = content.Property("timestamp");
+                //        if (timestampProperty != null && timestampProperty.Value.Type == JTokenType.Null)
+                //        {
+                //            timestampProperty.Value = DateTime.Now.ToString("O");
+                //            c.ApplicationMessage.Payload = Encoding.UTF8.GetBytes(content.ToString());
+                //        }
+                //    }
+                //    catch (Exception)
+                //    {
+                //    }
+                //};
+
+                mqttServer.ClientDisconnected += (s, e) =>
+                {
+                    Console.Write("Client disconnected event fired.");
+                };
+
+                await mqttServer.StartAsync(options);
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+
+                await mqttServer.StopAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class RetainedMessageHandler : IMqttServerStorage
+    {
+        private const string Filename = "C:\\MQTT\\RetainedMessages.json";
+
+        public Task SaveRetainedMessagesAsync(IList<MqttApplicationMessage> messages)
+        {
+            var directory = Path.GetDirectoryName(Filename);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            File.WriteAllText(Filename, JsonConvert.SerializeObject(messages));
+            return Task.FromResult(0);
+        }
+
+        public Task<IList<MqttApplicationMessage>> LoadRetainedMessagesAsync()
+        {
+            IList<MqttApplicationMessage> retainedMessages;
+            if (File.Exists(Filename))
+            {
+                var json = File.ReadAllText(Filename);
+                retainedMessages = JsonConvert.DeserializeObject<List<MqttApplicationMessage>>(json);
+            }
+            else
+            {
+                retainedMessages = new List<MqttApplicationMessage>();
+            }
+
+            return Task.FromResult(retainedMessages);
+        }
+    }
+}
